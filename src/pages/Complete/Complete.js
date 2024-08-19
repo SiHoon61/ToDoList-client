c169cc1ee010b548a9b888a8a711dbe987d8c531
SiHoon61 <dlatlgns000@gmail.com>
2024-08-19 21:44:01 +0000

    Fix: complete page

--- src/pages/Complete/Complete.js
+++ src/pages/Complete/Complete.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 const Complete = () => {
+    
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/payment/complete`, {}, {
+                    params: {
+                        menuName: "Menu1",
+                    },
+                });
+                console.log(response.data);
+            } catch (err) {
+                console.log(err.message);
+            }
+        };
+        fetchData(); // 컴포넌트 마운트 시 fetchData 함수 실행
+    }, []); // 빈 배열을 의존성 배열로 전달하여 한 번만 실행되도록 설정
     return (
         <>
             결제가 완료되었습니다. 감사합니다.
--- src/pages/Pay/Pay.js
+++ src/pages/Pay/Pay.js
@@ -8,15 +8,17 @@ import {
     Button,
     ButtonText
 } from './style';
+
 const Pay = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const menu = location.state;
+
     const fetchData = async () => {
         try {
             const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/payment/complete`, {}, {
                 params: {
-                    menuName: "Menu1",
+                    menuName: menu,
                 },
             });
             console.log(response.data);
@@ -25,9 +27,8 @@ const Pay = () => {
         }
     };
 
-
     const onClickPayment = () => {
-        // 아임포트 결제 모듈 불러오기
+        console.log(menu);
         const { IMP } = window;
         IMP.init(`${process.env.REACT_APP_IMP}`); // 포트원에서 발급받은 가맹점 식별코드 입력
 
@@ -36,43 +37,36 @@ const Pay = () => {
             pay_method: 'card', // 결제수단 (예: card, trans 등)
             merchant_uid: `mid_${new Date().getTime()}`, // 가맹점에서 생성한 고유 주문번호
             name: '천원의 아침밥',
-            amount: '1000', // 결제 금액
+            amount: '100', // 결제 금액
             buyer_email: 'test@test.com',
             buyer_name: '홍길동',
             buyer_tel: '010-1234-5678',
             buyer_addr: '서울특별시 강남구 삼성동',
             buyer_postcode: '123-456',
+            m_redirect_url: 'https://to-do-list-client-two.vercel.app/complete'
         };
 
-        IMP.request_pay(data, (response) => {
-            console.log(response); // 결제 응답 전체를 확인
-            const { success, error_msg } = response;
-            if (success) {
-                fetchData();
-                navigate('/complete');
-            } else {
-                alert(`결제에 실패하였습니다. 오류 내용: ${error_msg}`);
-            }
-        });
+        IMP.request_pay(data, callback);
     };
 
-    // const callback = (response) => {
-    //     const { success, error_msg } = response;
-    //     if (success) {
-    //         fetchData()
-    //         navigate('/complete');
-    //     } else {
-    //         alert(`결제에 실패하였습니다. 오류 내용: ${error_msg}`);
-    //     }
-    // };
+    const callback = (response) => {
+        const { success, error_msg } = response;
+        if (success) {
+            fetchData();
+        } else {
+            console.error(`결제에 실패하였습니다. 오류 내용: ${error_msg}`);
+        }
+        // 결제 결과와 관계없이 무조건 리다이렉트
+        navigate('/complete');
+    };
+
+    useEffect(() => {
+        onClickPayment();
+    }, []); // 컴포넌트가 마운트될 때 결제 실행
+
     return (
         <Container>
             <Title>결제가 진행중입니다. 잠시만 기다려주세요</Title>
-            <Button onClick={onClickPayment}>
-                <ButtonText>
-                    결제하기
-                </ButtonText>
-            </Button>
         </Container>
     );
 };
